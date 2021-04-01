dbd3571388abfc7e9d540d49e393d65264f3553f
bruno <enzo_jpn@hotmail.com>
2021-03-31 23:32:54 -0300

    exemplo de teste com mockito

---
+++ src/main/java/com/example/demo/api/controller/TestaMockito.java
@@ -0,0 +1,8 @@
+package com.example.demo.api.controller;
+
+public class TestaMockito {
+ 
+	public int soma(int a , int b) {
+		return a+b;
+	}
+}
--- src/test/java/com/example/demo/AlgaworksApiApplicationTests.java
+++ src/test/java/com/example/demo/AlgaworksApiApplicationTests.java
@@ -1,13 +1,26 @@
 package com.example.demo;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import com.example.demo.api.controller.TestaMockito;
 
 @SpringBootTest
 class AlgaworksApiApplicationTests {
 
 	@Test
 	void contextLoads() {
+		
+		TestaMockito m = mock(TestaMockito.class);
+		when(m.soma(1, 1)).thenReturn(11);
+		TestaMockito tm = new TestaMockito();  
+		int r = m.soma(1, 1);
+		r= tm.soma(1, 1);
+		assertEquals(2, r);
 	}
 
 }
